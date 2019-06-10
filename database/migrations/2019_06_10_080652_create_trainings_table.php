71c843d610098a715cdada48f9da15751e8b9edf
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-06-10 14:29:18 +0530

    Updated traning model

---
+++ database/migrations/2019_06_10_080652_create_trainings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingsTable extends Migration
+{
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::create('trainings', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('name');
+      $table->string('image');
+      $table->longtext('objective');
+      $table->longtext('outcomes');
+      $table->longtext('audience');
+      $table->longtext('curriculum');
+      $table->float('rating');
+      $table->integer('hours');
+      $table->integer('assignments');
+      $table->integer('students');
+      $table->enum('admissions', [0, 1]);
+      $table->timestamps();
+    });
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::dropIfExists('trainings');
+  }
+}
