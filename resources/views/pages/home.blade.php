0c0897af4569199509621b6ee22bd73177a07555
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-07-28 02:45:04 +0530

    Updated home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master')
 @section('pre')
   @php
-    $title = "Welcome to A.I statics technology";
+  $title = "Welcome to A.I statics technology";
   @endphp
 @endsection
 @section('content')
   <div class="intro intro-carousel">
     <div id="carousel" class="owl-carousel owl-theme">
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-1.jpg)">
+      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slider/slide1.jpg)">
         <div class="overlay overlay-a"></div>
         <div class="intro-content display-table">
           <div class="table-cell">
@@ -15,60 +15,32 @@
               <div class="row">
                 <div class="col-lg-8">
                   <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Mount
-                      <br> Olive Road Two</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-2.jpg)">
-        <div class="overlay overlay-a"></div>
-        <div class="intro-content display-table">
-          <div class="table-cell">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Rino
-                      <br> Venda Road Five</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item-a intro-item bg-image" style="background-image: url(img/slide-3.jpg)">
-        <div class="overlay overlay-a"></div>
-        <div class="intro-content display-table">
-          <div class="table-cell">
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-8">
-                  <div class="intro-body">
-                    <p class="intro-title-top">Doral, Florida
-                      <br> 78345</p>
-                    <h1 class="intro-title mb-4">
-                      <span class="color-b">204 </span> Alira
-                      <br> Roan Road One</h1>
-                    <p class="intro-subtitle intro-price">
-                      <a href="#"><span class="price-a">rent | $ 12.000</span></a>
-                    </p>
+                    <h1 class="intro-title mb-4 wow fadeInUp">
+                      Welcome to
+                      <span class="color-b">AI </span> Statics
+                    </h1>
+                    <ul class="list">
+                      @php
+                        $aitems = array(
+                          array('name'=>'Industry Solutions', 'img'=> 'industry'),
+                          array('name'=>'Corporate training', 'img'=> 'training'),
+                          array('name'=>'Project guidance', 'img'=> 'project'),
+                          array('name'=>'Certified Internship', 'img'=> 'certified')
+                        );
+                      @endphp
+                      @foreach ($aitems as $item)
+                        <li>
+                          <div class="list-item wow fadeInLeft" data-wow-delay="{{100*($loop->index+1)}}ms">
+                            <div class="list-img">
+                              <img src="{{url('/img/'.$item['img'].'.svg')}}"/>
+                            </div>
+                            <div class="list-text">
+                              {{$item['name']}}
+                            </div>
+                          </div>
+                        </li>
+                      @endforeach
+                    </ul>
                   </div>
                 </div>
               </div>
@@ -140,6 +112,4 @@
         </div>
       </section>
     </main>
-    <div style="height:1000px;">
-    </div>
   @endsection
