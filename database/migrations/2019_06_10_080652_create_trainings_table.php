97102e89ad1f6bd8e4e8beeece821b50f217f617
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-07-27 19:59:33 +0530

    Updated tranings table

--- database/migrations/2019_06_10_080652_create_trainings_table.php
+++ database/migrations/2019_06_10_080652_create_trainings_table.php
@@ -17,6 +17,7 @@ class CreateTrainingsTable extends Migration
       $table->bigIncrements('id');
       $table->string('name');
       $table->string('image');
+      $table->string('boucher');
       $table->longtext('objective');
       $table->longtext('outcomes');
       $table->longtext('audience');
